f50036cc1c3643b953b70e440a45ffbdfdb1b87d
phx <oldjamey@gmail.com>
2019-09-23 22:13:30 +0100

    made corrections

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 #DockerChroot
+
+This container acts as a secure userland jumpbox that you can NAT to port 2222 on the host.
+
+Modify install.sh with the correct CIDR range if you are not using 192.168.1.0/24.
+
+Container uses key-based authentication and will only have password-based SSH access to the the host with no access to the rest of the network.
+
+
+
+Run ./install.sh.
+
+Sudo will be required for some commands.
+
+Adding additional users is outside the scope of this project, but can easily be achieved by reading through and modifying the Dockerfile.
--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ ssh-keygen -f ~/.ssh/known_hosts -R [127.0.0.1]:2222
 ssh-keygen -q -f ./id_rsa -N '' -t rsa -b 2048
 chmod 600 id_rsa.pub
 chmod 600 id_rsa
-sudo iptables-save > "/etc/iptables/iptables.${timestamp}.rules"
-echo 'sudo iptables -A INPUT -i docker0 -p tcp --dport 22 -j ACCEPT' | sudo tee /etc/iptables/dockerchroot.rules
-echo 'sudo iptables -I FORWARD -i docker0 -d 192.168.1.0/24 -j REJECT' | sudo tee -a /etc/iptables/dockerchroot.rules
-cat /etc/iptables/dockerchroot.rules | sudo iptables-restore
-echo 'If you need to restore you previous iptables rules,'
-echo "run 'sudo iptables-restore < /etc/iptables/iptables.${timestamp}.rules'"
+sudo iptables-save | sudo tee "/etc/iptables/iptables.${timestamp}.rules" > /dev/null 2>&1
+sudo iptables -A INPUT -i docker0 -p tcp --dport 22 -j ACCEPT
+sudo iptables -I FORWARD -i docker0 -d 192.168.1.0/24 -j REJECT
+sudo iptables-save | sudo tee /etc/iptables/dockerchroot.rules > /dev/null 2>&1
+echo -e '\nIf you need to restore you previous iptables rules,'
+echo -e "run 'sudo iptables-restore < /etc/iptables/iptables.${timestamp}.rules'\n"
 
 # Build Docker Image
 read -p 'Enter new root password for container: ' rootpasswd
@@ -24,15 +24,9 @@ docker stop chroot 2>/dev/null
 docker rm chroot 2>/dev/null
 docker rmi chroot 2>/dev/null
 docker build -t chroot .
-echo
-echo 'Running container:'
-docker run -dp 127.0.0.1:2222:2222 --name chroot chroot
+rm -f passwd rootpasswd
+echo -e '\nRunning container:'
+docker run --restart=always -dp 0.0.0.02222:2222 --name chroot chroot
 docker ps -a | grep chroot
-echo
-echo "Adding container's public key to ${HOME}/.ssh/authorized_keys..."
-docker cp chroot:/home/alpine/.ssh/id_rsa.pub chroot.rsa.pub
-cat chroot.rsa.pub | tee -a "${HOME}/.ssh/authorized_keys"
-chmod 700 "${HOME}/.ssh"
-chmod 600 "${HOME}/.ssh/authorized_keys"
-rm -v chroot.rsa.pub
-echo -e '\nDone.'
+echo -e '\nExec into the container: docker exec -it chroot /bin/sh'
+echo 'SSH into the container: ssh -i id_rsa -p 2222 alpine@localhost'
