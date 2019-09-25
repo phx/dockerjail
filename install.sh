7e9ac59b273e75f10f9bdd1412bd4563801eed74
phx <oldjamey@gmail.com>
2019-09-25 20:33:02 +0100

    added environment variable support for CIDR range, and smart iptables location lookup

--- install.sh
+++ install.sh
@@ -33,6 +33,15 @@ elif [[ -n $(echo $* | grep help) ]]; then
   show_help && exit
 fi
 
+if [[ -z $CIDR ]]; then
+  if [[ -n $(echo $* | grep interactive) ]]; then
+    read -p 'Please enter your network in CIDR notation: [192.168.1.0/24] ' CIDR
+      if [[ -z $(echo "$CIDR" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,3}') ]]; then CIDR='192.168.1.0/24'; fi
+  else
+    CIDR='192.168.1.0/24'
+  fi
+fi
+
 if [[ -n $(echo $* | grep remove) ]]; then
   sudo iptables-restore 2>/dev/null < /etc/iptables/iptables.beforedockerjail.rules
   sudo rm -vf /etc/iptables/dockerjail.rules
@@ -43,12 +52,11 @@ if [[ -n $(echo $* | grep remove) ]]; then
   sudo rm -vf /lib/systemd/system/docker.service.bak
   sudo systemctl daemon-reload
   sudo systemctl restart docker
-  sudo docker stop jail 2>/dev/null
-  sudo docker rm jail 2>/dev/null
-  sudo docker rmi jail 2>/dev/null
+  sudo docker stop dockerjail 2>/dev/null
+  sudo docker rm dockerjail 2>/dev/null
+  sudo docker rmi dockerjail 2>/dev/null
   ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222 2>/dev/null
   ssh-keygen -f ~/.ssh/known_hosts -R [127.0.0.1]:2222 2>/dev/null
-  # sudo systemctl restart networking
   echo 'Removed.'
   exit
 fi
@@ -58,14 +66,15 @@ ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222 2>/dev/null
 ssh-keygen -f ~/.ssh/known_hosts -R [127.0.0.1]:2222 2>/dev/null
 ssh-keygen -q -f ./id_rsa -N '' -t rsa -b 2048
 chmod 600 id_rsa.pub
-chmod 600 id_rsa
+chmod 400 id_rsa
 
 # Configure iptables to act right:
+iptables_restore="$(command -v iptables-restore)"
 sudo iptables-save | sudo tee "/etc/iptables/iptables.beforedockerjail.rules" > /dev/null 2>&1
 sudo iptables -w -A INPUT -i docker0 -p tcp --dport 2222 -j ACCEPT
-sudo iptables -w -I FORWARD -i docker0 -d 192.168.1.0/24 -j REJECT
+sudo iptables -w -I FORWARD -i docker0 -d "${CIDR}" -j REJECT
 sudo iptables-save | sudo tee /etc/iptables/dockerjail.rules > /dev/null 2>&1
-echo -e '#!/bin/bash\n\nsudo /sbin/iptables-restore < /etc/iptables/dockerjail.rules' | sudo tee /usr/local/bin/dockerjailrules > /dev/null 2>&1
+echo -e '#!/bin/sh\n\nsudo ${iptables_restore} < /etc/iptables/dockerjail.rules' | sudo tee /usr/local/bin/dockerjailrules > /dev/null 2>&1
 sudo chmod +x /usr/local/bin/dockerjailrules
 sudo cp -pv /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
 sudo sed -i 's@containerd.sock@containerd.sock\nExecStartPost=/usr/local/bin/dockerjailrules@' /lib/systemd/system/docker.service
@@ -89,13 +98,13 @@ if [[ -z $USERPASS ]]; then
 fi
 echo "root:${ROOTPASS}" > rootpasswd
 echo "alpine:${USERPASS}" > passwd
-sudo docker stop jail 2>/dev/null
-sudo docker rm jail 2>/dev/null
-sudo docker rmi jail 2>/dev/null
-sudo docker build -t jail .
+sudo docker stop dockerjail 2>/dev/null
+sudo docker rm dockerjail 2>/dev/null
+sudo docker rmi dockerjail 2>/dev/null
+sudo docker build -t dockerjail .
 rm -f passwd rootpasswd
 echo -e '\nRunning container:'
-sudo docker run --restart=always -dp 2222:2222 --name jail jail
+sudo docker run --restart=always -dp 2222:2222 --name dockerjail dockerjail
 echo 'Restarting docker...'
 sudo systemctl restart docker.service
 if [[ $? -ne 0 ]]; then
@@ -103,8 +112,8 @@ if [[ $? -ne 0 ]]; then
   exit
 fi
 sudo /usr/local/bin/dockerjailrules
-sudo docker ps -a | grep jail
-echo -e "\nExec into the container: docker exec -it jail /bin/sh"
+sudo docker ps -a | grep dockerjail
+echo -e "\nExec into the container: docker exec -it dockerjail sh"
 echo -e "SSH into the container from the host: ssh -i id_rsa -p 2222 alpine@localhost"
 echo -e "Note: The container all local network access to this container is REJECTED,"
 echo -e "but if you NAT to port 2222 on this host, you will be able to access via the following command:"
