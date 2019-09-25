1b13861636819e90607a700ed8c2365e15bde79c
phx <oldjamey@gmail.com>
2019-09-25 17:27:44 +0100

    updated script to actually work.

--- scripts/regenerate_keys.sh
+++ scripts/regenerate_keys.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
+echo -e 'ssh_host key checksums:\n' &&\
+old_host_checksums=`for i in /etc/ssh/ssh_host*; do echo "${i}: \`sha256sum $i\`"; done` &&\
+echo "$old_host_checksums" &&\ 
+echo -e '\nssh private keypair checksums:\n' &&\
+old_keypair_checksums=`for i in /home/alpine/.ssh/id_rs*; do echo "${i}: \`sha256sum $i\`"; done` &&\
+echo -e "$old_keypair_checksums\n" 
+
+# regenerate keys:
+rm -vf /etc/ssh/ssh_host* &&\
 ssh-keygen -A &&\
+ls -lha /etc/ssh/ssh_host*
+exit
+
 rm -vf /home/alpine/.ssh/id_rsa /home/alpine/.ssh/id_rsa.pub /home/alpine/.ssh/known_hosts&&\
 ssh-keygen -q -f /home/alpine/.ssh/id_rsa -N '' -t rsa -b 2048 &&\
 chmod 600 /home/alpine/.ssh/id_rsa.pub &&\
 chmod 400 /home/alpine/.ssh/id_rsa &&\
+
+echo -e 'ssh_host key checksums:\n' &&\
+new_host_checkums=`for i in /etc/ssh/ssh_host*; do echo "${i}: \`sha256sum $i\`"; done` &&\
+echo "$new_host_checksums" &&\
 cat /home/alpine/.ssh/id_rsa.pub > /home/alpine/.ssh/known_hosts &&\
+echo -e '\nssh private keypair checksums:\n' &&\
+new_keypair_checksums=`for i in /home/alpine/.ssh/id_rs*; do echo "${i}: \`sha256sum $i\`"; done` &&\
+echo -e "$new_keypair_checksums\n" &&\
 chmod 700 /home/alpine/.ssh &&\
 chmod 600 /home/alpine/.ssh/known_hosts &&\
+
+echo -e 'SSH KEY OLD/NEW CHECKSUM COMPARISON:'
+diff -y <(echo "$old_host_checksums") <(echo "$new_host_checksums")
+diff -y <(echo "$old_keypair_checksums") <(echo "$new_keypair_checksums")
+
 echo -e "
 
 Your ssh host keys and private keypair have been regenerated.
@@ -28,5 +52,4 @@ you can remotely SSH directly into the container via the following command:
 (substitute 2222 with whatever external port you have NAT'd to the dockerjail host port 2222).
 
 "
-
 
