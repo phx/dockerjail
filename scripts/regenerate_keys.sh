cc021917740fb9a54627011b72286cf52127616b
phx <oldjamey@gmail.com>
2019-09-25 18:49:44 +0100

    fixed script

--- scripts/regenerate_keys.sh
+++ scripts/regenerate_keys.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
 
-echo -e 'ssh_host key checksums:\n' &&\
-old_host_checksums=`for i in /etc/ssh/ssh_host*; do echo "${i}: \`sha256sum $i\`"; done` &&\
-echo "$old_host_checksums" &&\ 
-echo -e '\nssh private keypair checksums:\n' &&\
-old_keypair_checksums=`for i in /home/alpine/.ssh/id_rs*; do echo "${i}: \`sha256sum $i\`"; done` &&\
+echo -e '\ncurrent /etc/ssh/* checksums:' 
+old_host_checksums=`for i in /etc/ssh/*; do sha256sum $i; done` 
+echo "$old_host_checksums" 
+echo -e '\ncurrent ~/.ssh/* checksums:' 
+old_keypair_checksums=`for i in /home/alpine/.ssh/*; do sha256sum $i; done` 
 echo -e "$old_keypair_checksums\n" 
 
 # regenerate keys:
-rm -vf /etc/ssh/ssh_host* &&\
-ssh-keygen -A &&\
-ls -lha /etc/ssh/ssh_host*
-exit
+rm -vf /etc/ssh/ssh_host*
+echo 
+ssh-keygen -A
+echo 
+rm -vf /home/alpine/.ssh/id_rsa /home/alpine/.ssh/id_rsa.pub /home/alpine/.ssh/known_hosts 
+ssh-keygen -q -f /home/alpine/.ssh/id_rsa -N '' -t rsa -b 2048 
+chmod 600 /home/alpine/.ssh/id_rsa.pub 
+chmod 400 /home/alpine/.ssh/id_rsa 
 
-rm -vf /home/alpine/.ssh/id_rsa /home/alpine/.ssh/id_rsa.pub /home/alpine/.ssh/known_hosts&&\
-ssh-keygen -q -f /home/alpine/.ssh/id_rsa -N '' -t rsa -b 2048 &&\
-chmod 600 /home/alpine/.ssh/id_rsa.pub &&\
-chmod 400 /home/alpine/.ssh/id_rsa &&\
+echo -e '\nnew /etc/ssh/* checksums:'
+new_host_checksums=`for i in /etc/ssh/*; do sha256sum $i; done` 
+echo "$new_host_checksums" 
+cat /home/alpine/.ssh/id_rsa.pub > /home/alpine/.ssh/known_hosts 
+echo -e '\nnew ~/.ssh/* checksums:' 
+new_keypair_checksums=`for i in /home/alpine/.ssh/*; do sha256sum $i; done` 
+echo -e "$new_keypair_checksums\n" 
+chmod 700 /home/alpine/.ssh 
+chmod 600 /home/alpine/.ssh/known_hosts
 
-echo -e 'ssh_host key checksums:\n' &&\
-new_host_checkums=`for i in /etc/ssh/ssh_host*; do echo "${i}: \`sha256sum $i\`"; done` &&\
-echo "$new_host_checksums" &&\
-cat /home/alpine/.ssh/id_rsa.pub > /home/alpine/.ssh/known_hosts &&\
-echo -e '\nssh private keypair checksums:\n' &&\
-new_keypair_checksums=`for i in /home/alpine/.ssh/id_rs*; do echo "${i}: \`sha256sum $i\`"; done` &&\
-echo -e "$new_keypair_checksums\n" &&\
-chmod 700 /home/alpine/.ssh &&\
-chmod 600 /home/alpine/.ssh/known_hosts &&\
-
-echo -e 'SSH KEY OLD/NEW CHECKSUM COMPARISON:'
-diff -y <(echo "$old_host_checksums") <(echo "$new_host_checksums")
-diff -y <(echo "$old_keypair_checksums") <(echo "$new_keypair_checksums")
+echo 'SSH KEY OLD/NEW CHECKSUM COMPARISON:'
+tmp=`mktemp -d`
+echo "$old_host_checksums" > "${tmp}/oldkeys"
+echo "$old_keypair_checksums" >> "${tmp}/oldkeys"
+echo "$new_host_checksums" > "${tmp}/newkeys"
+echo "$new_keypair_checksums" >> "${tmp}/newkeys"
+diff "${tmp}/oldkeys" "${tmp}/newkeys"
+rm -rf "${tmp}"
 
 echo -e "
 
@@ -52,4 +56,3 @@ you can remotely SSH directly into the container via the following command:
 (substitute 2222 with whatever external port you have NAT'd to the dockerjail host port 2222).
 
 "
-
